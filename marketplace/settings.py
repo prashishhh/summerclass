b2905ba82bc3b4c12afead3c36e4b1b59ffc8cff
Prashish Sapkota <155043124+prashishhh@users.noreply.github.com>
2025-08-08 10:27:24 +0545

    Final updates to settings.py and Pipfile.lock

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -31,7 +31,9 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'summerclass-ghxx.onrender.com',  # Render URL
     '127.0.0.1',                      # for local development
-    'localhost',                     # optional
+    'localhost',   
+    'prashishsapkota.com.np'
+    'www.prashishsapkota.com.np'                  # optional
 ]
 
 
@@ -88,18 +90,18 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL')
+#     )
+# }
 
 
 
